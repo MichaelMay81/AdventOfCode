853389c2e7f094d98e3b020177907702a6b2fbaa
Michael May <may@vrvis.at>
2020-12-08 16:15:15 +0100

    Some basic unit testing with xunit and unquote

--- Program.fs
+++ Program.fs
@@ -13,55 +13,55 @@ let testAll () =
 
     printfn "day1-2 Test:  %A" (Day1.puzzle2 input)
     printfn "day1-2 Final: %A" (getPuzzleInput "Inputs/Day1.txt" |> Seq.toList |> List.map int |> Day1.puzzle2)
-    
-    let input = ["1-3 a: abcde"; "foobar"; "1-3 b: cdefg"; "2-9 c: ccccccccc"] |> List.toSeq
-    printfn "day2-1 Test:  %A" (Day2.puzzle1 input)
-    printfn "day2-1 Final: %A" (getPuzzleInput "Inputs/Day2.txt" |> Day2.puzzle1)
-    
-    printfn "day2-2 Test:  %A" (Day2.puzzle2 input)
-    printfn "day2-2 Final: %A" (getPuzzleInput "Inputs/Day2.txt" |> Day2.puzzle2)
+//    
+//    let input = ["1-3 a: abcde"; "foobar"; "1-3 b: cdefg"; "2-9 c: ccccccccc"] |> List.toSeq
+//    printfn "day2-1 Test:  %A" (Day2.puzzle1 input)
+//    printfn "day2-1 Final: %A" (getPuzzleInput "Inputs/Day2.txt" |> Day2.puzzle1)
+//    
+//    printfn "day2-2 Test:  %A" (Day2.puzzle2 input)
+//    printfn "day2-2 Final: %A" (getPuzzleInput "Inputs/Day2.txt" |> Day2.puzzle2)
     
 //    printfn "foobar %A" (["1-3 a: abcde" |> Day2.parse] |> filter |> Seq.map Day2.isPassCorrect2)
 //    printfn "foobar %A" (["1-3 b: cdefg" |> Day2.parse] |> filter |> Seq.map Day2.isPassCorrect2)
 //    printfn "foobar %A" (["2-9 c: ccccccccc" |> Day2.parse] |> filter |> Seq.map Day2.isPassCorrect2)
-
-    let input = [|"..##......."
-                  "#...#...#.."
-                  ".#....#..#."
-                  "..#.#...#.#"
-                  ".#...##..#."
-                  "..#.##....."
-                  ".#.#.#....#"
-                  ".#........#"
-                  "#.##...#..."
-                  "#...##....#"
-                  ".#..#...#.#"|]
-    
-    printfn "day3-1 Test:  %A" (Day3.puzzle1 input)
-    printfn "day3-1 Final:  %A" (getPuzzleInput "Inputs/Day3.txt" |> Seq.toArray |> Day3.puzzle1)
-    
-    printfn "day3-2 Test:  %A" (Day3.puzzle2 input)
-    printfn "day3-2 Final:  %A" (readLines "Inputs/Day3.txt" |> Result.map ( Seq.toArray >> Day3.puzzle2))
-
-    let input = [|"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd"
-                  "byr:1937 iyr:2017 cid:147 hgt:183cm"
-                  ""
-                  "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884"
-                  "hcl:#cfa07d byr:1929"
-                  ""
-                  "hcl:#ae17e1 iyr:2013"
-                  "eyr:2024"
-                  "ecl:brn pid:760753108 byr:1931"
-                  "hgt:179cm"
-                  ""
-                  "hcl:#cfa07d eyr:2025 pid:166559648"
-                  "iyr:2011 ecl:brn hgt:59in"|]
-    
-    printfn "day4-1 Test:  %A" (Day4_1.puzzle input)
-    printfn "day4-1 Final:  %A" (readLines "Inputs/Day4.txt" |> Result.map Day4_1.puzzle)
+//
+//    let input = [|"..##......."
+//                  "#...#...#.."
+//                  ".#....#..#."
+//                  "..#.#...#.#"
+//                  ".#...##..#."
+//                  "..#.##....."
+//                  ".#.#.#....#"
+//                  ".#........#"
+//                  "#.##...#..."
+//                  "#...##....#"
+//                  ".#..#...#.#"|]
+//    
+//    printfn "day3-1 Test:  %A" (Day3.puzzle1 input)
+//    printfn "day3-1 Final:  %A" (getPuzzleInput "Inputs/Day3.txt" |> Seq.toArray |> Day3.puzzle1)
+//    
+//    printfn "day3-2 Test:  %A" (Day3.puzzle2 input)
+//    printfn "day3-2 Final:  %A" (readLines "Inputs/Day3.txt" |> Result.map ( Seq.toArray >> Day3.puzzle2))
+//
+//    let input = [|"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd"
+//                  "byr:1937 iyr:2017 cid:147 hgt:183cm"
+//                  ""
+//                  "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884"
+//                  "hcl:#cfa07d byr:1929"
+//                  ""
+//                  "hcl:#ae17e1 iyr:2013"
+//                  "eyr:2024"
+//                  "ecl:brn pid:760753108 byr:1931"
+//                  "hgt:179cm"
+//                  ""
+//                  "hcl:#cfa07d eyr:2025 pid:166559648"
+//                  "iyr:2011 ecl:brn hgt:59in"|]
+//    
+//    printfn "day4-1 Test:  %A" (Day4_1.puzzle input)
+//    printfn "day4-1 Final:  %A" (readLines "Inputs/Day4.txt" |> Result.map Day4_1.puzzle)
 
     //printfn "day4-2 Test:  %A" (Day4_2.puzzle input)
-    printfn "day4-2 Final:  %A" (readLines "Inputs/Day4.txt" |> Result.map Day4_2.puzzle)
+//    printfn "day4-2 Final:  %A" (readLines "Inputs/Day4.txt" |> Result.map Day4_2.puzzle)
     
 //    printfn "test %A" (Day4_2.parseYear 1920 2002 "2002")
 //    printfn "test %A" (Day4_2.parseYear 1920 2002 "2003")
@@ -131,8 +131,8 @@ let main argv =
                   "dark blue bags contain 2 dark violet bags."
                   "dark violet bags contain no other bags."]
 
-//    printfn "day7-1 Test:  %A" (input1 |> (Day7.puzzle1 "shiny gold"))
-//    printfn "day7-1 Final:  %A" (readLines "Inputs/Day7.txt" |> Result.map (Day7.puzzle1 "shiny gold"))
+    printfn "day7-1 Test:  %A" (input1 |> (Day7.puzzle1 "shiny gold"))
+    printfn "day7-1 Final:  %A" (readLines "Inputs/Day7.txt" |> Result.map (Day7.puzzle1 "shiny gold"))
     
     printfn "day7-2 Test1:  %A" (input1 |> (Day7.puzzle2 "shiny gold"))
     printfn "day7-2 Test2:  %A" (input2 |> (Day7.puzzle2 "shiny gold"))
