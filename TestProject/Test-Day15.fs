e28dbe51203cde95b21915e20d62fd4233f91269
Michael May <may@vrvis.at>
2020-12-15 09:31:56 +0100

    Day 15 Puzzle 2, and fsharplint.json (for fsharplint, which was added last commit)

--- TestProject/Test-Day15.fs
+++ TestProject/Test-Day15.fs
@@ -7,6 +7,8 @@ open AoC_Mike
 
 let getPuzzleInput = Program.getPuzzleInput
 
+// Puzzle 1
+
 [<Fact>]
 let ``Test Day 15/1 Test1`` () =
     0 =! ([0; 3; 6] |> Day15.puzzle1 10)
@@ -43,3 +45,36 @@ let ``Test Day 15/1 Test8`` () =
 let ``Test Day 15/1 Final`` () =
     203 =! ([0; 5; 4; 1; 10; 14; 7] |> Day15.puzzle1 2020)
     
+// Puzzle 2
+    
+[<Fact>]
+let ``Test Day 15/2 Test2`` () =
+    175594 =! ([0; 3; 6] |> Day15.puzzle1 30000000)
+
+[<Fact>]
+let ``Test Day 15/2 Test3`` () =
+    2578 =! ([1; 3; 2] |> Day15.puzzle1 30000000)
+     
+[<Fact>]
+let ``Test Day 15/2 Test4`` () =
+    3544142 =! ([2; 1; 3] |> Day15.puzzle1 30000000)
+     
+[<Fact>]
+let ``Test Day 15/2 Test5`` () =
+    261214 =! ([1; 2; 3] |> Day15.puzzle1 30000000)
+     
+[<Fact>]
+let ``Test Day 15/2 Test6`` () =
+    6895259 =! ([2; 3; 1] |> Day15.puzzle1 30000000)
+    
+[<Fact>]
+let ``Test Day 15/2 Test7`` () =
+    18 =! ([3; 2; 1] |> Day15.puzzle1 30000000)
+     
+[<Fact>]
+let ``Test Day 15/2 Test8`` () =
+    362 =! ([3; 1; 2] |> Day15.puzzle1 30000000)
+
+[<Fact>]
+let ``Test Day 15/2 Final`` () =
+    9007186 =! ([0; 5; 4; 1; 10; 14; 7] |> Day15.puzzle1 30000000)
